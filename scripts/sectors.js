f6beb35358028867f70c5bd53107bcc397210b38
紺鳥 <122795383+CondoriNavyBlue@users.noreply.github.com>
2026-07-11 20:28:37 +0900

    Update sectors.js

--- scripts/sectors.js
+++ scripts/sectors.js
@@ -2,8 +2,7 @@
 const IronValley = extend(SectorPreset, "IronValley", Planets.serpulo, 218,{
     localizedName: "Iron Valley",
     difficulty: 3.9,
-    captureWave: 50,
-    
+    captureWave: 50
 });
 
 module.exports = {
