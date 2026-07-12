380c041d9ef3969f30112e377ab36142d6401e52
紺鳥 <122795383+CondoriNavyBlue@users.noreply.github.com>
2026-07-12 15:46:49 +0900

    Update sectors.js

--- scripts/sectors.js
+++ scripts/sectors.js
@@ -4,7 +4,6 @@ const IronValley = extend(SectorPreset, "IronValley", Planets.serpulo, 218,{
     difficulty: 3.9,
     captureWave: 50
 });
-IronValley.generator = new FileMapGenerator(IronValley);
 
 module.exports = {
     IronValley: IronValley
