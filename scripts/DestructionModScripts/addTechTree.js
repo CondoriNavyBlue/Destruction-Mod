3d0127ae8f024c19bbb55c35d192c60dad1bd924
紺鳥 <122795383+CondoriNavyBlue@users.noreply.github.com>
2026-08-26 11:52:19 +0900

    test

--- scripts/DestructionModScripts/addTechTree.js
+++ scripts/DestructionModScripts/addTechTree.js
@@ -2,20 +2,43 @@ const addTechTree = (content, research) => {
     if (!content) {
         throw new Error('子内容为空!,');
     }
+
     if (!research.parent) {
         throw new Error('研究。父母为空!');
     }
+
     var researchName = research.parent;
     var customRequirements = research.requirements;
     var objectives = research.objectives;
 
     var lastNode = TechTree.all.find(boolf(t => t.content == content));
+
     if (lastNode != null) {
         lastNode.remove();
     }
 
-    var node = new TechTree.TechNode(null, content, customRequirements !== undefined ? customRequirements : content.researchRequirements());
+    var requirements;
+
+    if (customRequirements !== undefined) {
+        requirements = customRequirements.map(req =>
+            new ItemStack(req.item, req.amount)
+        );
+    } else {
+        var originalRequirements = content.researchRequirements();
+
+        requirements = originalRequirements.map(req =>
+            new ItemStack(req.item, req.amount)
+        );
+    }
+
+    var node = new TechTree.TechNode(
+        null,
+        content,
+        requirements
+    );
+
     var currentMod = Vars.mods.getMod("destructionmod");
+
     if (objectives) {
         node.objectives.addAll(objectives);
     }
@@ -24,25 +47,29 @@ const addTechTree = (content, research) => {
         node.parent.children.remove(node);
     }
 
-    // find parent node.
-    var parent = TechTree.all.find(boolf(t => t.content.name.equals(researchName) || t.content.name.equals(currentMod.name + "-" + researchName)));
+    var parent = TechTree.all.find(
+        boolf(t =>
+            t.content.name.equals(researchName) ||
+            t.content.name.equals(currentMod.name + "-" + researchName)
+        )
+    );
 
-    // if (parent == null) {
-    //     throw new Error("Content '" + researchName + "' isn't in the tech tree, but '" + content.name + "' requires it to be researched.");
-    // }
     if (parent == null) {
-        throw new Error("'内容 '" + researchName + "' 不在科技树上, 但 '" + content.name + "'需要对其研究.");
+        throw new Error(
+            "'内容 '" +
+            researchName +
+            " 不在科技树上, 但 '" +
+            content.name +
+            "'需要对其研究."
+        );
     }
-    // add this node to the parent
+
     if (!parent.children.contains(node)) {
         parent.children.add(node);
     }
-    // reparent the node
+
     node.parent = parent;
 };
-
-//DaRealMonika's Code
-//Thank you
 
 
 const items = require("DestructionModScripts/items");
