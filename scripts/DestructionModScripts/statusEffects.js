27688497fc6d2c35f638e191ef06f882d33302a8
紺鳥 <122795383+CondoriNavyBlue@users.noreply.github.com>
2026-09-10 18:19:44 +0900

    Update statusEffects.js

--- scripts/DestructionModScripts/statusEffects.js
+++ scripts/DestructionModScripts/statusEffects.js
@@ -16,8 +16,8 @@ const Destructed = extend(StatusEffect,"Destructed",{
         unit.reloadMultiplier *= 0.5;
     }
 });
-Destructed.stats.addMultModifier(extend(Stat, "Damage Multiplier", {localized(){return "Damage Multiplier";}}), 0.75);
-Destructed.stats.addMultModifier(extend(Stat, "Health Multiplier", {localized(){return "Health Multiplier";}}), 0.75);
+Destructed.stats.addMultModifier(extend(Stat, "Damage Multiplier", {localized(){return "Damage Multiplier";}}), 0.5);
+Destructed.stats.addMultModifier(extend(Stat, "Health Multiplier", {localized(){return "Health Multiplier";}}), 0.5);
 Destructed.stats.addMultModifier(extend(Stat, "Speed Multiplier", {localized(){return "Speed Multiplier";}}), 0.5);
 Destructed.stats.addMultModifier(extend(Stat, "Reload Multiplier", {localized(){return "Reload Multiplier";}}), 0.5);
 Destructed.stats.add(extend(Stat, "Damage", {localized(){return "Damage";}}), 1000, StatUnit.perSecond);
